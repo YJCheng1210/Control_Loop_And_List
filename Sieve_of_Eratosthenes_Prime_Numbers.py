aabb1529fdee226f7f935ef84824e746d58e25e4
Yan Jhong Cheng <108399737+YJCheng1210@users.noreply.github.com>
2022-07-12 11:25:35 +0800

    Update Sieve_of_Eratosthenes_Prime_Numbers.py

--- Sieve_of_Eratosthenes_Prime_Numbers.py
+++ Sieve_of_Eratosthenes_Prime_Numbers.py
@@ -1,11 +1,8 @@
-from math import sqrt
-
 num = int(input('Enter a number: '))
 nums = range(2, num+1)
-end_num = int(sqrt(nums[-1])) + 1
 trial = 0
 
-while nums[trial] < end_num:
+while trial < round(len(nums)/2):
     new_nums = [i for i in nums if i==nums[trial] or i%nums[trial]>0]
     nums = new_nums
     trial += 1
